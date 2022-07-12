a4396afd3e7be3688265f0d61beb90ec2189cbb7
AtilaSaraiva <atilasaraiva@gmail.com>
2022-07-12 17:58:47 -0300

    added chmod for nix-portable and put the download inside a if

--- codigos/prep.sh
+++ codigos/prep.sh
@@ -1,2 +1,6 @@
-wget https://github.com/DavHau/nix-portable/releases/download/v009/nix-portable
+nixportable='nix-portable'
+if [ ! -f $nixportable ]; then
+    wget https://github.com/DavHau/nix-portable/releases/download/v009/nix-portable
+    chmod +x nix-portable
+fi
 ./nix-portable nix-shell
