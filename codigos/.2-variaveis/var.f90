c36809febcf96c6eb26d377db776c15f510c643f
Átila Saraiva <atilasaraiva@gmail.com>
2022-07-12 18:57:51 -0300

    added complex.f90

---
+++ codigos/.2-variaveis/var.f90
@@ -0,0 +1,14 @@
+program variaveis
+    implicit none
+
+    real             :: numeroDecimal = 3.141592
+    integer          :: numeroInteiro = 3
+    character        :: caractere     = "a"
+    character(len=5) :: nome          = "atila"
+
+    print*, "Número real: ", numeroDecimal
+    print*, "Número inteiro: ", numeroInteiro
+    print*, "Caractere único: ", caractere
+    print*, "String: ", nome
+
+end program variaveis
---
+++ codigos/2-variaveis/complex.f90
@@ -0,0 +1,14 @@
+! Escreva aqui um código que declare
+! dois números complexos,
+! inicialize eles
+! faça todas as operações aritiméticas básicas sobre eles
+! imprima os resultados
+
+program numerosComplexos
+    implicit none
+
+    complex, parameter :: i = (0, 1)   ! sqrt(-1)
+
+
+
+end program numerosComplexos
