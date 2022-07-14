d53bc594f24dd3c70b31df164904077b7abfb466
Átila Saraiva <atilasaraiva@gmail.com>
2022-07-13 21:32:10 -0300

    added matplotlib and numpy to the dev env

--- codigos/shell.nix
+++ codigos/shell.nix
@@ -5,8 +5,17 @@ with import sources.nixpkgs {
   ];
 };
 
+let
+  my-python-packages = python-packages: with python-packages; [
+    numpy
+    matplotlib
+    # other python packages you want
+  ];
+  python-with-my-packages = python3.withPackages my-python-packages;
+in
 mkShell {
   buildInputs = [
+    python-with-my-packages
     gfortran
     fortran-fpm
     notepadqq
